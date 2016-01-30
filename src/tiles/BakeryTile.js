c0b46888d1596250f1236400653d6a1f14cb7201
Pablo Lubitz <plubitz@uni-bremen.de>
2016-01-30 23:10:03 +0100

    Bäckerei ohne Bild

---
+++ src/tiles/BakeryTile.js
@@ -0,0 +1,27 @@
+"use strict";
+
+class BakeryTile extends Tile {
+    constructor(row, column) {
+        super("baeckerei.gif", row, column);
+		this.build_costs = {humansidle: 10};
+        this.tooltip_name = "BAKERY";
+        this.counter = 40;
+    }
+
+    clone() {
+        return new BakeryTile(this.row, this.column);
+    }
+
+    step() {
+        this.counter = this.counter - 1;
+        if (this.counter > 0) {
+            return {};
+        }
+        this.counter = 40;
+        if(resources['humansidle']>5){
+            return {
+                "breads": 10,"flour":-20,
+            };
+        }
+    }
+};
