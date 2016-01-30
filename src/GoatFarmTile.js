a92229ce7577ed1b790e3a40aca5c97ffcb3b7a4
Pablo Lubitz <plubitz@uni-bremen.de>
2016-01-30 18:00:05 +0100

    goat produktion
    Signed-off-by: Pablo Lubitz <plubitz@uni-bremen.de>

--- src/GoatFarmTile.js
+++ src/GoatFarmTile.js
@@ -3,8 +3,20 @@
 class GoatFarmTile extends Tile {
     constructor(row, column) {
         super("ziegenhof.gif", row, column);
+        this.count = 100;
     }
     clone() {
         return new GoatFarmTile(this.row, this.column);
+    }
+
+    step() {
+        this.counter = this.counter - 1;
+        if (this.counter > 0) {
+            return {};
+        }
+        this.counter = 100;
+        return {
+            "goats": 1,
+        };
     }
 };
