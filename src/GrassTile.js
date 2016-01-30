74ef106a30b8458e6e8e91044395cfa7127706c4
Lucas Klemmer <klemmer@uni-bremen.de>
2016-01-30 15:21:53 +0100

    Ziegenhof mit animation

--- src/GrassTile.js
+++ src/GrassTile.js
@@ -2,7 +2,7 @@
 
 class GrassTile extends Tile {
     constructor(row, column) {
-        super("farm.png", row, column);
+        super("ziegenhof.gif", row, column);
         this.clickable = true;
     }
 };
