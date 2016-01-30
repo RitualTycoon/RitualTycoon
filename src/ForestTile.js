73ad58a9cb6bf2c16bb9631f9bc5e2a7564907f3
Pablo Lubitz <plubitz@uni-bremen.de>
2016-01-30 16:37:35 +0100

    Forest->Grass

--- src/ForestTile.js
+++ src/ForestTile.js
@@ -4,6 +4,9 @@ class ForestTile extends Tile {
     constructor(row, column) {
         super("wald.png", row, column);
     }
+    getUpgrades() {
+        return [new GrassTile()];
+    }
     clone() {
         return new ForestTile(this.row, this.column);
     }
