c8644b395c86eb2eb5c8dbc5ae4d4850fb28f789
Jan Niklas Hasse <jhasse@gmail.com>
2016-01-31 13:15:48 +0100

    Produktionskosten etwas angepasst

--- src/tiles/FieldTile.js
+++ src/tiles/FieldTile.js
@@ -4,7 +4,7 @@ class FieldTile extends Tile {
     constructor(row, column) {
         super("weizenfarm.png", row, column);
 		this.build_costs = {humansidle: 2, water: 15};
-        this.production = {wheat: 5,};
+        this.production = { wheat: 1, };
         this.adjacent_needs = [ new WellTile() ];
 		this.tooltip_name = "FIELD \n needs to be next to Well";
         this.counter = 30;
--- src/tiles/WindmillTile.js
+++ src/tiles/WindmillTile.js
@@ -4,7 +4,7 @@ class WindmillTile extends Tile {
     constructor(row, column) {
         super("muele.gif", row, column);
 		this.build_costs = {humansidle: 2, water: 30, wheat:15};
-        this.production = {/*wheat: -2, */flour: 5};
+        this.production = { flour: 1 };
         this.adjacent_needs = [ new FieldTile() ];
         this.tooltip_name = "wINDMILL \n needs to be next to Field";
         this.counter = 30;
@@ -14,7 +14,7 @@ class WindmillTile extends Tile {
     clone() {
         return new WindmillTile(this.row, this.column);
     }
-    
+
     getUpgrades() {
         return [new GrassTile(),]
     }
