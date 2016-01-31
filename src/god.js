ad97ec6347cb0ca542d144efda4ae4bc342621a3
Jannik <jannik@somyeol.com>
2016-01-31 13:30:48 +0100

    sacrifice humans

--- src/god.js
+++ src/god.js
@@ -25,8 +25,6 @@ class Quests {
                 let td2 = document.createElement("td");
                 table.appendChild(tr);
                 tr.appendChild(td1);
-                // TODO: Fürs Menschen opfern auskommentieren:
-                // tr.appendChild(td2);
 
                 //Opferbutton und Menschenopferbutton
                 let inner_div = document.createElement("div");
@@ -34,25 +32,10 @@ class Quests {
                 inner_div.innerText = this._quest[text] + "x " + text;
                 if(this._quest[text] > resources[text])
                     tr.setAttribute('style', 'opacity: 0.3;');
-                let img = document.createElement("img");
-                img.id = "security_humans";
-
                 td1.addEventListener("click", function () {
                     that.ritual();
                 });
-
-                img.addEventListener("click", function () {
-                    //TODO: Menschen opfern
-                    //if (resources['humansidle'] < foodValue[text] )
-                    //    return;
-                    //that._quest[text] -= 1;
-                    //that._questValue -= 1;
-                    //resources['humansidle'] -= 1;
-                });
-
-                //Buttons in die Tabelle einhengen
                 td1.appendChild(inner_div);
-                td2.appendChild(img);
             }
         }
 
@@ -75,9 +58,11 @@ class Quests {
         for (var key in this._quest) {
             if (resources[key] < this._quest[key]){
                 if(sacrificeHumans){
-                    if(resources['humansidle'] >= this._quest[key]){
-                        resources['humansidle'] = resources['humansidle'] - this._quest[key];
-                        this._sacrificed['humans'] = this._sacrificed['humans'] ? this._sacrificed['humans'] + this._quest[key] : this._quest[key];
+                    let humanValue = Math.ceil((foodValue[key]/100) * this._quest[key]);
+                    if(resources['humansidle'] >= humanValue){
+                        console.log('Sacrificing '+humanValue+' humans')
+                        resources['humansidle'] = resources['humansidle'] - humanValue;
+                        this._sacrificed['humans'] = this._sacrificed['humans'] ? this._sacrificed['humans'] + humanValue : humanValue;
                         this.increaseDifficulty(this._difficulty*0.01*this._quest[key]);
                         continue;
                     }
