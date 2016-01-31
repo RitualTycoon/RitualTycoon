9aa67bf0f864ddab6c174f241e1716b5f98dd5a7
Jan Niklas Hasse <jhasse@gmail.com>
2016-01-31 13:22:07 +0100

    Bugfix

--- src/main.js
+++ src/main.js
@@ -138,7 +138,7 @@ window.onload = function() {
 setInterval(function() {
     for (let row of board.board) {
         for (let tile of row) {
-            let newResources = tile.suspended ?  tile.step() : {};
+            let newResources = tile.suspended ? {} : tile.step();
             tile.counter -= 1;
             if (tile.counter < 0) tile.counter = tile.workingspeed;
             for (let newResource in newResources) {
