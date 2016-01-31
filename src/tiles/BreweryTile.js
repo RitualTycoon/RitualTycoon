12720a7165cf44a540002349ddf7c8ddc5741851
Jan Niklas Hasse <jhasse@gmail.com>
2016-01-31 10:39:13 +0100

    Bier-Kosten auch erstmal auskommentiert

--- src/tiles/BreweryTile.js
+++ src/tiles/BreweryTile.js
@@ -4,7 +4,11 @@ class BreweryTile extends Tile {
     constructor(row, column) {
         super("brauerei.gif", row, column);
 		this.build_costs = {humansidle: 6, water: 100, wheat:30};
-        this.production_needs = {water: -2, wheat: -2, beer: 1};
+
+        // Dass Gebäude etwas benötigen, habe ich erstmal auskommentiert. Es ist noch
+        // zu verbuggt (negatives Mehl) und wird grafisch nicht deutlich.
+        this.production_needs = {/*water: -2, wheat: -2, */beer: 1};
+
         this.tooltip_name = "BREWERY";
         this.counter = 40;
         this.workingspeed = 40;
